074bb7e1ae3fc944e3fca57f2ae24e227becd6ae
Manan Patel <mananpatel4925@gmail.com>
2025-12-18 10:52:59 +0530

    Update axiosClient.js

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://homigo-bac-45.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
